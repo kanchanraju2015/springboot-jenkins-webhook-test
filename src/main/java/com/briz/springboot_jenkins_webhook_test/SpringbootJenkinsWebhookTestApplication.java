a29f945993ad4243975e3d6467b5d12105ab4352
kanchan raju <kanchanraju2015@gmail.com>
2025-04-28 16:35:17 +0530

    first jenkins commit

---
+++ src/main/java/com/briz/springboot_jenkins_webhook_test/SpringbootJenkinsWebhookTestApplication.java
@@ -0,0 +1,27 @@
+package com.briz.springboot_jenkins_webhook_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class SpringbootJenkinsWebhookTestApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(SpringbootJenkinsWebhookTestApplication.class, args);
+	}
+@RequestMapping("/test")
+public String test()
+{
+	return "this is jenkins webhook test";
+}
+@RequestMapping("/jenkins")
+public String test1()
+{
+	return "this is jenkins webhook test example";
+}
+
+}
---
+++ src/test/java/com/briz/springboot_jenkins_webhook_test/SpringbootJenkinsWebhookTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.briz.springboot_jenkins_webhook_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJenkinsWebhookTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

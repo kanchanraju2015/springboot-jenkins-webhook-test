98d6006404c4d1ad6e6efb1b8f8d2c08a74b3cc8
Kanchan Raju <kanchanraju2015@gmail.com>
2025-05-01 14:34:33 +0530

    Update SpringbootJenkinsWebhookTestApplication.java

--- src/main/java/com/briz/springboot_jenkins_webhook_test/SpringbootJenkinsWebhookTestApplication.java
+++ src/main/java/com/briz/springboot_jenkins_webhook_test/SpringbootJenkinsWebhookTestApplication.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 	// hello jenkins
+	// hello jenkins report 
 public class SpringbootJenkinsWebhookTestApplication {
 
 	public static void main(String[] args)
